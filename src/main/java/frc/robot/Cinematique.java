90a829b5048d8b1dc2914d53782c807131a54022
nadineducegep <nadineducegep@gmail.com>
2024-01-15 23:51:46 -0500

    Interfaces pour les configurations

---
+++ src/main/java/frc/robot/Cinematique.java
@@ -0,0 +1,107 @@
+package frc.robot;
+
+//import java.util.Map;
+//import static java.util.Map.entry; 
+
+public interface Cinematique {
+
+    public interface Roues
+    {
+      public double FACTEUR_ROUES = 0.7;
+    }
+    public interface Manette
+    {
+      enum Direction {DEVANT, DERRIERE, LATERAL_DROIT, LATERAL_GAUCHE, ROTATION_DROITE, ROTATION_GAUCHE};
+
+      public static final double SEUIL_ZERO = 0.08;//0.02;
+      public static final double SEUIL_AXES_OPPOSES = 0.3;
+      //static double BIAIS_AXE_GAUCHE_X = 0.06299212574958801;
+      //static double BIAIS_AXE_GAUCHE_Y = -0.06299212574958801;
+      static double BIAIS_AXE_GAUCHE_X = 0;
+      static double BIAIS_AXE_GAUCHE_Y = -0;
+    } 
+    /*
+    public interface Bras
+    {
+      // B = 0 - A = 6.5 - X = 13
+      // arriere pour scorer- centrer pour deplacement - devant pour ramasser
+      public static final float POSITION_AVANT = 11f;
+      public static final float POSITION_PENCHE_AVANT = 8.5f;
+      public static final float POSTIION_MILIEU = 6.1f;
+      public static final float POSITION_PENCHE_ARRIERE = 4f;
+      public static final float POSITION_ARRIERE = 0.5f;
+      public enum POSITION {POSITION_AVANT, POSITION_PENCHE_AVANT, POSTIION_MILIEU, POSITION_PENCHE_ARRIERE, POSITION_ARRIERE, AJUSTEE, INCONNUE};
+      public static final double POSITION_INCONNUE = -1;
+      Map<POSITION, Float> POSITION_NUMERIQUE = Map.ofEntries(
+        entry(POSITION.POSITION_AVANT, POSITION_AVANT),
+        entry(POSITION.POSITION_PENCHE_AVANT, POSITION_PENCHE_AVANT),
+        entry(POSITION.POSTIION_MILIEU, POSTIION_MILIEU),
+        entry(POSITION.POSITION_PENCHE_ARRIERE, POSITION_PENCHE_ARRIERE),
+        entry(POSITION.POSITION_ARRIERE, POSITION_ARRIERE));
+
+      //public static final double P = 0.05;
+      //public static final double I = 0.000001;//0.0001;  // gros = bang bang
+      public static final double P = 0.1;
+      public static final double I = 0.000002;//0.0001;  // gros = bang bang
+      public static final double D = 8;
+      public static final double MAX = 0.3;
+
+      public static double SEUIL_GRAND_MOUVEMENT = 10;
+      public static double SEUIL_PETIT_MOUVEMENT = 4;
+      public interface GRAND_MOUVEMENT
+      {
+        public static final double P = 0.1;
+        public static final double I = 0.000002;//0.0001;  // gros = bang bang
+        public static final double D = 8;
+        public static final double MAX = 0.3;  
+      }
+      public interface MOYEN_MOUVEMENT
+      {
+        public static final double P = 0.1;
+        public static final double I = 0.000002;//0.0001;  // gros = bang bang
+        public static final double D = 8;
+        public static final double MAX = 0.3;  
+      }
+      public interface PETIT_MOUVEMENT
+      {
+        public static final double P = 0.1;
+        public static final double I = 0.000002;//0.0001;  // gros = bang bang
+        public static final double D = 8;
+        public static final double MAX = 0.3;  
+      }
+      public interface INCREMENT
+      {
+        public static final double P = 0.2;
+        public static final double I = 0;
+        public static final double D = 0;
+        public static final double MAX = 0.3;  
+      }
+
+      public static int INTERVALLE_IMMOBILITE = 250;
+      public static double TEMPS_MAXIMUM_CALIBRER = 800;
+      public static double TEMPS_MAXIMUM_CALIBRER_AVANT = 1600;
+      public static double DISTANCE_NULLE = 2; 
+      // TODO peut-etre un ratio sur la distance
+
+
+    
+    }
+    public interface Machoire
+    {
+      public double VITESSE = 1;
+      public double VITESSE_OUVRIR = 0.5;
+      public double VITESSE_FERMER = 0.5;
+      public double TEMPS_MAXIMUM_OUVRIR = 1000; // ms
+      public double TEMPS_MAXIMUM_FERMER = 1000; // ms
+    }*/
+    
+}
+/**
+ * 
+       public static final float POSITION_AVANT = 12.75f;
+      public static final float POSTIION_MILIEU = 6.5f;
+      public static final float POSITION_ARRIERE = 0.875f;
+      public static final float POSITION_PENCHE_AVANT = 8.5f;
+      public static final float POSITION_PENCHE_ARRIERE = 4.5f;
+
+ */
---
+++ src/main/java/frc/robot/Materiel.java
@@ -0,0 +1,70 @@
+package frc.robot;
+import java.util.Map;
+import static java.util.Map.entry; 
+
+public interface Materiel {
+
+    public static int INTERRUPTEUR_DROIT = 1;
+    public static int INTERRUPTEUR_GAUCHE = 2;
+
+    public interface Affichage
+    {
+      public static int SIGNAL_ANIMATION_LED = 1; // et plusieurs autres pour les modes
+    }
+
+    public interface Roues
+    {
+        public int ROUE_AVANT_DROITE = 4; // ID 4 // roule avant
+        public int ROUE_ARRIERE_DROITE = 3; // ID 3 // roule avant
+        public int ROUE_AVANT_GAUCHE = 1; // ID 1  // roule arriere
+        public int ROUE_ARRIERE_GAUCHE = 2; // ID 2 // roule arriere
+    }
+    public interface Manette
+    {
+      public static final int MANETTE = 0;
+
+      public static final int BOUTON_A = 1;
+      public static final int BOUTON_B = 2;
+      public static final int BOUTON_X = 3;		
+      public static final int BOUTON_Y = 4;
+      public static final int BOUTON_MAIN_GAUCHE = 5; 
+      public static final int BOUTON_MAIN_DROITE = 6;
+      public static final int BOUTON_RETOUR = 7;
+      public static final int BOUTON_DEMARRER = 8;
+    
+      public static final int BATON_GAUCHE_AXE_X = 0;
+      public static final int BATON_GAUCHE_AXE_Y = 1;
+      public static final int BATON_DROIT_AXE_X = 4;    
+      public static final int BATON_DROIT_AXE_Y = 5;
+      public static final int MAIN_GAUCHE_AXE = 2;
+      public static final int MAIN_DROITE_AXE = 3;
+
+      public enum ANGLE {HAUT, HAUT_DROIT, DROIT, BAS_DROIT, BAS, BAS_GAUCHE, GAUCHE, HAUT_GAUCHE};
+        
+      Map<ANGLE, Integer> ANGLE_POV = Map.ofEntries(
+            entry(ANGLE.HAUT, 0),
+            entry(ANGLE.HAUT_DROIT, 45),
+            entry(ANGLE.DROIT, 90),
+            entry(ANGLE.BAS_DROIT, 135),
+            entry(ANGLE.BAS, 180),
+            entry(ANGLE.BAS_GAUCHE, 225),
+            entry(ANGLE.GAUCHE, 270),
+            entry(ANGLE.HAUT_GAUCHE, 315)
+            );
+    
+    }
+    /*
+    public interface Bras
+    {
+      public static final int MOTEUR_PRINCIPAL = 5; // tourne vers la batterie
+      public static final int MOTEUR_SECONDAIRE = 6; // tourne vers la batterie
+
+      public void abaisser();
+    }
+    public interface Machoire
+    {
+      public static final int MOTEUR = 7;
+    }
+    */
+    
+}
