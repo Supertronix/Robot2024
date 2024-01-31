21cf934a30d49cce560542f4e3a05f582ce80470
nadineducegep <nadineducegep@gmail.com>
2024-01-30 19:54:15 -0500

    Code de manette

--- src/main/java/frc/robot/RobotControleur.java
+++ src/main/java/frc/robot/RobotControleur.java
@@ -3,80 +3,65 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commande.MouvementDuRobot;
+import frc.robot.soussysteme.*;
+import frc.robot.interaction.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class RobotControleur extends TimedRobot {
 
   private Robot robot;
-  private Command trajetAutonome;
+  private Manette manette;
+  //private Command trajetAutonome;
 
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    robot = new Robot();
+    robot = new Robot(); //         this.robot = Robot.getInstance();
+    this.manette = Manette.getInstance();
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  // This runs after the mode specific periodic functions, but before LiveWindow and SmartDashboard integrated updating.
+  // polling buttons // adding newly-scheduled commands, // running already-scheduled commands, removing finished or interrupted commands // running subsystem periodic() methods.  
+  // This must be called from the robot's periodic block in order for anything in the Command-based framework to work.
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link Robot} class. */
   @Override
   public void autonomousInit() {
     //trajetAutonome = new CommandeTrajetAutonome();
     //trajetAutonome.schedule();
   }
-
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when teleop starts running. 
-    if (trajetAutonome != null) {
-      trajetAutonome.cancel();
-    }
+
+    System.out.println("teleopInit()");
+    ((RouesMecanumSynchro)Robot.getInstance().roues).convertirEnRouesHolonomiques();
+    ((RouesMecanumSynchro)Robot.getInstance().roues).setFacteur(0.8);
+    //    if (trajetAutonome != null) {
+     // trajetAutonome.cancel();
+    //}
   }
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //System.out.println("teleopPeriodic()");   
+    
+    robot.roues.conduireAvecManette(this.manette);
+    manette.executerActions();    
+  }
 
   @Override
   public void testInit() {
@@ -84,35 +69,18 @@ public class RobotControleur extends TimedRobot {
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
   
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   @SuppressWarnings({"unused"})
   private void lierInteractions() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(robot.partie::exampleCondition).onTrue(new MouvementDuRobot(robot.partie));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
 }
