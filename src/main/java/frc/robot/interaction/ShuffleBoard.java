49e757030fc98fa1dbaa6fe95c0ae8f9eb068d79
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2024-02-21 01:37:10 -0500

    Ajout alliance dans dashboard

--- src/main/java/frc/robot/interaction/ShuffleBoard.java
+++ src/main/java/frc/robot/interaction/ShuffleBoard.java
@@ -27,6 +27,7 @@ public class ShuffleBoard {
         SmartDashboard.putNumber("RPM Lanceur Maitre", 0);
         SmartDashboard.putNumber("RPM Lanceur Esclave", 0);
         SmartDashboard.putBoolean("Note chargée", false);
+        SmartDashboard.putBoolean("Alliance Rouge", true);
 
         SmartDashboard.putData("Commande Avaler", new CommandeAvaler());
         SmartDashboard.putData("Commande Lancer Haut", new CommandeLancerHaut());
@@ -36,6 +37,7 @@ public class ShuffleBoard {
         SmartDashboard.putNumber("RPM Lanceur Maitre", encodeurMaitreLanceur.getVelocity());
         SmartDashboard.putNumber("RPM Lanceur Esclave", encodeurEsclaveLanceur.getVelocity());
         SmartDashboard.putBoolean("Note chargée", capteurLuminosite.getLuminosite());
+        SmartDashboard.putBoolean("Alliance Rouge", Robot.getInstance().getEstAllianceRouge());
     }
 
     /**
