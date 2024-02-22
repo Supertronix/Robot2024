4809601e800ab5a133ddbdc8d03c4e3bae55194f
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2024-02-21 22:01:10 -0500

    x, y et angle controleur init

--- src/main/java/frc/robot/ControleurDeTrajectoires.java
+++ src/main/java/frc/robot/ControleurDeTrajectoires.java
@@ -1,10 +1,16 @@
 package frc.robot;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 
 public class ControleurDeTrajectoires {
 
     private HolonomicDriveController m_driveController;
+    private PIDController xControleur;
+    private PIDController yControleur;
+    private ProfiledPIDController angleControleur;
     public ControleurDeTrajectoires() {
+        m_driveController = new HolonomicDriveController(xControleur, yControleur, angleControleur);
     }
 }
