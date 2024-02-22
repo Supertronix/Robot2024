3bf78fb464c9b4ad6fcdd1f3370abba953c4f1c7
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2024-02-21 23:08:07 -0500

    typo

--- src/main/java/frc/robot/ControleurDeTrajectoires.java
+++ src/main/java/frc/robot/ControleurDeTrajectoires.java
@@ -6,11 +6,12 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 
 public class ControleurDeTrajectoires {
 
-    private HolonomicDriveController m_driveController;
+    private HolonomicDriveController driveController;
     private PIDController xControleur;
     private PIDController yControleur;
     private ProfiledPIDController angleControleur;
     public ControleurDeTrajectoires() {
-        m_driveController = new HolonomicDriveController(xControleur, yControleur, angleControleur);
+        driveController = new HolonomicDriveController(xControleur, yControleur, angleControleur);
+        // c'est un test, en réalité, les trajectoires seront défini en commandes
     }
 }
