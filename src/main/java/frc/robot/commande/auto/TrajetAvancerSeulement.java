e1b5da791cd42666b09bedeebcee2997a2970416
nadineducegep <nadineducegep@gmail.com>
2024-02-24 10:33:31 -0500

    Paquetages pour les command-group de trajet autonomes + exemples simplistes

---
+++ src/main/java/frc/robot/commande/auto/TrajetAvancerSeulement.java
@@ -0,0 +1,12 @@
+package frc.robot.commande.auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class TrajetAvancerSeulement extends SequentialCommandGroup{
+	
+	public TrajetAvancerSeulement()
+	{
+		//addSequential(new CommandeRouesAvancer(5350));
+	}
+	
+}
