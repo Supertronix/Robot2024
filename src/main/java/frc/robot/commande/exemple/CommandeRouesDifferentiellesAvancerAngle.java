c777aa002ae7e54eee3976a8f5b3e62480c42a9a
nadineducegep <nadineducegep@gmail.com>
2024-02-25 14:02:57 -0500

    Ancien exemples de commandes

---
+++ src/main/java/frc/robot/commande/exemple/CommandeRouesDifferentiellesAvancerAngle.java
@@ -0,0 +1,98 @@
+package frc.robot.commande.exemple;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+
+public class CommandeRouesDifferentiellesAvancerAngle extends Command{
+
+	protected double distanceVoulue;
+	protected double positionInitiale = 0;
+	protected double vitesse = 0.1; // test primitif
+	protected boolean PIDFirstLoop = true;
+	protected double angleVoulue;
+	
+	//protected InterpreteurMouvementCumulateur verificateurImmobilite;
+	
+	public CommandeRouesDifferentiellesAvancerAngle(double distanceVoulue, double angle)
+	{
+		//requires(Robot.getInstance().roues);		
+		this.distanceVoulue = distanceVoulue;
+		this.angleVoulue = angle;
+	}
+
+	@Override
+	public void initialize() {
+		//verificateurImmobilite = new InterpreteurMouvementCumulateur();
+		//Robot.roues.setGyroPid(RobotMap.Roues.GYRO_KP_AVANCER_ANGLE, RobotMap.Roues.GYRO_KI_AVANCER_ANGLE);
+		//Robot.roues.zeroSensors();
+		//Robot.roues.setGyroConsigne(angleVoulue);
+		System.out.println("CommandeRouesAvancer.initialize()");
+		//this.positionInitiale = Robot.roues.getDistanceGauche();
+		System.out.println("Position initiale " + positionInitiale);
+		
+		// Robot.roues.avancer(vitesse); 
+		// Robot.roues.informerEncodeurDroitDeLaCible(distanceVoulue);
+	}
+
+	@Override
+	public void execute() {
+		
+		
+		//if (Math.abs(distanceVoulue - Robot.roues.getDistanceGauche()) <= 500)
+		{
+			if (PIDFirstLoop)
+			{
+					//Robot.roues.resetPIDS();
+					PIDFirstLoop = false;
+			}
+			
+			//Robot.roues.setDistanceConsigne(distanceVoulue);
+			//Robot.roues.conduireDroit();
+		}
+		//else
+		{
+			if (distanceVoulue > 0)
+			{
+				//Robot.roues.conduireDroitGyroSeul(0.75);
+			}
+			else
+			{
+				//Robot.roues.conduireDroitGyroSeul(-0.75);
+			}
+		}
+		//this.verificateurImmobilite.mesurer();
+
+		//System.out.println("CommandeRouesAvancer.execute()");
+		//Robot.roues.avancer(vitesse); 
+		//Robot.roues.avancer(Robot.roues.getVitesseSelonEncodeurDroitStabilise()); 
+		// System.out.println("selon pid vitesse = "  + Robot.roues.getVitesseSelonEncodeurDroitStabilise());
+	}
+	
+	@Override
+	public boolean isFinished() {
+		boolean estFini = false;
+		
+		if (distanceVoulue >= 0)
+		{
+			//estFini = Robot.roues.getDistanceGauche() >= (this.distanceVoulue);
+		}
+		else
+		{
+			//estFini = Robot.roues.getDistanceGauche() <= (this.distanceVoulue);
+		}
+		
+		//System.out.println("Distance parcourue " + (Robot.roues.getDistanceGauche() - this.positionInitiale));
+		//System.out.println("isFinished() " + estFini);
+		//return estFini || this.verificateurImmobilite.estImmobile();
+		return estFini;
+	}
+	
+	//@Override
+	//public void end() {
+	//	System.out.println("CommandeRouesAvancer.end()");
+		//Robot.roues.disablePIDs();
+		
+		//Robot.roues.arreter();
+	//}
+	
+}
---
+++ src/main/java/frc/robot/commande/exemple/CommandeRouesDifferentiellesTourner.java
@@ -0,0 +1,91 @@
+package frc.robot.commande.exemple;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.soussysteme.Roues;
+import frc.robot.Robot;
+
+public class CommandeRouesDifferentiellesTourner extends Command{
+
+	protected double angleVoulue;
+	protected boolean PIDFirstLoop = true;
+	
+	protected double angleInitiale = 0;
+	protected boolean negativeAngle = false;
+	
+	public CommandeRouesDifferentiellesTourner(double angleVoulue)
+	{
+		//requires(Robot.getInstance().roues);		
+		//this.angleVoulue = angleVoulue;
+		//if (angleVoulue < 0) negativeAngle = true;
+	}
+
+	@Override
+	public void initialize()  {
+		if (Math.abs(angleVoulue) > 45)
+		{
+		//	Robot.roues.setGyroPid(RobotMap.Roues.GYRO_KP_ROTATEONLY, RobotMap.Roues.GYRO_KI_ROTATEONLY_ANGLE_ELEVE);
+		}
+		else
+		{
+		//	Robot.roues.setGyroPid(RobotMap.Roues.GYRO_KP_ROTATEONLY, RobotMap.Roues.GYRO_KI_ROTATEONLY);
+		}
+	
+		//Robot.roues.zeroSensors();
+		//Robot.roues.setGyroConsigne(angleVoulue);
+	}
+
+	@Override
+	public void execute() {
+		
+		//if (Math.abs(angleVoulue - Robot.getInstance().roues.getGyroAngle()) <= 15)
+		{
+			if (PIDFirstLoop)
+			{
+				//Robot.roues.resetPIDS();
+				PIDFirstLoop = false;
+			}
+			//Robot.roues.setGyroConsigne(angleVoulue);
+			//Robot.roues.rotateWithGyro();
+		}
+		//else
+		{
+			if (angleVoulue > 0)
+			{
+				//Robot.roues.conduire(0.65, -0.65);
+			}
+			else
+			{
+				//Robot.roues.conduire(-0.65, 0.65);
+			}
+		}
+		
+	}
+	
+	@Override
+	public boolean isFinished() {
+		boolean estFini = false;
+		
+		if (negativeAngle)
+		{
+			//estFini = Robot.roues.getGyroAngle() <= (this.angleVoulue);
+		}
+		else
+		{
+			//estFini = Robot.roues.getGyroAngle() >= (this.angleVoulue);
+		}
+			
+		//if (estFini) Robot.roues.conduire(0,0);
+		
+		//System.out.println("Angle parcourue " + (Robot.roues.getGyroAngle() - this.angleInitiale));
+		//System.out.println("isFinished() " + estFini);
+		return estFini;
+	}
+	
+	//public void end() {
+	//	System.out.println("CommandeRouesAvancer.end()");
+		//Robot.roues.disablePIDs();
+		
+		//Robot.roues.arreter();
+	//}
+	
+
+}
