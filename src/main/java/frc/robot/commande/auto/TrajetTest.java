df121a619dcf8d2fec73a6f53c25e4a914d46f01
razor7877 <razor7877@gmail.com>
2024-02-29 02:36:51 -0500

    Continuer travail CommandeTourner avec PID

--- src/main/java/frc/robot/commande/auto/TrajetTest.java
+++ src/main/java/frc/robot/commande/auto/TrajetTest.java
@@ -3,6 +3,7 @@ package frc.robot.commande.auto;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commande.terrain.classique.CommandeAvancer;
 import frc.robot.commande.terrain.classique.CommandeDeplacer;
+import frc.robot.commande.terrain.classique.CommandeTourner;
 
 public class TrajetTest extends SequentialCommandGroup {
     
@@ -13,7 +14,8 @@ public class TrajetTest extends SequentialCommandGroup {
         //     new CommandeDeplacer(500, 0, 0, 0.3)
         // );a
         addCommands(
-            new CommandeAvancer(30)
+            //new CommandeAvancer(-30)
+            new CommandeTourner(90)
         );
     }
     
--- src/main/java/frc/robot/commande/terrain/classique/CommandeTourner.java
+++ src/main/java/frc/robot/commande/terrain/classique/CommandeTourner.java
@@ -1,7 +1,9 @@
 package frc.robot.commande.terrain.classique;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
+import frc.robot.interaction.LecteurAccelerometre;
 import frc.robot.soussysteme.Roues;
 import frc.robot.mesure.LimiteurDuree;
 
@@ -9,18 +11,27 @@ public class CommandeTourner extends Command {
 
     private static final int TEMPS_MAXIMUM = 3000;
 
-    protected Roues roues = null;
-    protected boolean finie = false;
+    protected Roues roues;
+    protected boolean finie;
     protected LimiteurDuree detecteur;
-    protected double angle; // angle en degre ???
+    protected LecteurAccelerometre gyroscope;
 
-    public CommandeTourner(double angle)
+    protected PIDController pid;
+
+    protected double angleCible; // angle en degre ???
+    protected double angleActuel;
+
+    public CommandeTourner(double angleCible)
     {
         //System.out.println("new CommandeTourner()");
-        this.angle = angle;
         this.roues = Robot.getInstance().roues;
         this.addRequirements(this.roues);
         this.detecteur = new LimiteurDuree(TEMPS_MAXIMUM);
+        this.gyroscope = LecteurAccelerometre.getInstance();
+        this.pid = new PIDController(0.001, 0.00, 0.00);
+
+        this.angleCible = angleCible;
+        this.angleActuel = gyroscope.getYaw();
     }
        
     @Override
@@ -31,13 +42,23 @@ public class CommandeTourner extends Command {
         //this.roues.avancer(pas);
         this.detecteur.initialiser();
         this.finie = false;
-    }
-    @Override
-    public void execute() {
-        System.out.println("CommandeTourner.execute()");
-        this.detecteur.mesurer();
+        pid.reset();
     }
 
+    @Override
+    public void execute() {
+        //System.out.println("CommandeTourner.execute()");
+        this.angleActuel = gyroscope.getYaw();
+        double vitesseRotation = pid.calculate(this.angleActuel, this.angleCible);
+
+        if (this.angleActuel > this.angleCible)
+            this.roues.tournerGauche(vitesseRotation);
+        else
+            this.roues.tournerDroite(vitesseRotation);
+
+        System.out.println("Yaw gyro: " + gyroscope.getYaw() + " - Yaw target: " + this.angleCible);
+        this.detecteur.mesurer();
+    }
     
     /** 
      * @return boolean
@@ -45,11 +66,20 @@ public class CommandeTourner extends Command {
     @Override
     public boolean isFinished() 
     {
-        //if(this.estArrivee() || this.detecteur.estTropLongue())
-        return true;
+        boolean seuilAngleAtteint = Math.abs(this.angleActuel - this.angleCible) < 5.0;
+
+        if (seuilAngleAtteint)
+            return true;
+
+        if(this.detecteur.estTropLongue())
+            return true;
+        
+        return false;
     }
+
     @Override
     public void end(boolean interrupted) {
         System.out.println("CommandeAvancer.end()");
+        this.roues.arreter();
     }
 }
